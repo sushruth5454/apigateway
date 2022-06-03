6e47f61c0c1e2358fe7843a09ae5e43ab81f18b3
Sushruth <nsushrut@ford.com>
2022-06-03 14:56:20 +0530

    Initial commit

---
+++ api-gateway/src/main/java/com/apigateway/controller/FallbackController.java
@@ -0,0 +1,17 @@
+package com.apigateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/userServiceFallback")
+    public String userServiceFallback(){
+        return "user service is down at this time";
+    }
+    @GetMapping("/contactServiceFallback")
+    public String contactServiceFallback(){
+        return "contact serivce is down at this time";
+    }
+}
